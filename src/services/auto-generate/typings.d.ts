3bb38d7a19c476d3af8eb45d62038e31034ccc0f
dengyali0125 <dengyali0125@outlook.com>
2023-12-19 16:56:47 +0800

    feat: add auto generate list

---
+++ src/services/auto-generate/typings.d.ts
@@ -0,0 +1,12 @@
+
+declare namespace AutoGenerate {
+    type CreateOptions = {
+        id: null | string;
+        baseInfo: string;
+        apiData: string,
+        funcFormationData: string,
+        funcData: string,
+        docsData: string,
+        templateData: string
+    };
+}
