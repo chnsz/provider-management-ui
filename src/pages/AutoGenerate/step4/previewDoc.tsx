beb147847be7b1713b437a913223712e16d3ae68
dengyali0125 <dengyali0125@outlook.com>
2023-12-19 17:22:08 +0800

    feat: add auto generate otherStep

---
+++ src/pages/AutoGenerate/step4/previewDoc.tsx
@@ -0,0 +1,55 @@
+import { Col, Row, Space, Tabs } from 'antd';
+import React, { useEffect, useState } from 'react';
+import CodeEditor from "@/components/CodeEditor";
+const { TabPane } = Tabs;
+
+export type exampleDate = {
+    title: string;
+    script: string;
+};
+
+const PreviewDoc: React.FC<{ previewDataPar: any }> = ({ previewDataPar }) => {
+    const [codeContent, setCodeContent] = useState<string>();
+    const [docsContent, setDocsContent] = useState<string>();
+    const [testCodeContent, setTestCodeContent] = useState<string>();
+    const defaultActiveKey = 'tab1';
+
+    useEffect(() => {
+        setCodeContent(previewDataPar?.codeContent);
+        setDocsContent(previewDataPar?.docsContent);
+        setTestCodeContent(previewDataPar?.testCodeContent);
+    }, [previewDataPar]);
+
+
+    return <>
+        <Space direction={"vertical"} size={20} style={{ width: '100%' }}>
+            <Row>
+                <Col flex="100%">
+                    <div className={'portal-card'}>
+                        <div className={'header'}>预览</div>
+                        <div className={'container'}>
+                            {
+                                !previewDataPar ?
+                                    <div className={'no-data'}>暂无文档</div> :
+                                    <Tabs defaultActiveKey={defaultActiveKey}>
+                                        <TabPane tab="代码" key="tab1">
+                                            <CodeEditor language={'go'} readOnly value={codeContent} />
+                                        </TabPane>
+
+                                        <TabPane tab="文档" key="tab2">
+                                            <CodeEditor language={'go'} readOnly value={docsContent} />
+                                        </TabPane>
+                                        <TabPane tab="单元测试" key="tab3">
+                                            <CodeEditor language={'go'} readOnly value={testCodeContent} />
+                                        </TabPane>
+                                    </Tabs>
+                            }
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Space>
+    </>
+}
+
+export default PreviewDoc;
